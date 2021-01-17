fc19e157741b4a93ee9dda0bc9a095c5827383f7
Dylan Fontana <dyfontana@gmail.com>
2021-01-17 14:50:56 -0500

    Rust (#2)

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,8 @@
+docker-compose build disbot
+docker save -o disbot.tar disbot:latest
 ssh $USER@raspberrypi 'mkdir -p ~/deploy'
-scp -r docker $USER@raspberrypi:~/deploy/
+scp disbot.tar $USER@raspberrypi:~/deploy/
 scp docker-compose.yaml $USER@raspberrypi:~/deploy/
-scp -r src $USER@raspberrypi:~/deploy/
-ssh $USER@raspberrypi 'cd ~/deploy && docker-compose up -d --build'
+scp prod.env $USER@raspberrypi:~/deploy/
+rm disbot.tar
+ssh $USER@raspberrypi 'cd ~/deploy && docker load -i disbot.tar && docker-compose up -d && docker image prune -fa'
---
+++ src/config.rs
@@ -0,0 +1,32 @@
+use std::{env, env::VarError};
+
+pub struct Config {
+  api_key: String,
+  emote_name: String,
+  emote_users: Vec<String>,
+}
+
+impl Config {
+  pub fn new() -> Result<Config, VarError> {
+    Ok(Config {
+      api_key: env::var("API_KEY")?,
+      emote_name: env::var("EMOTE_NAME")?,
+      emote_users: env::var("EMOTE_USERS")?
+        .split(",")
+        .map(|x| x.to_string())
+        .collect(),
+    })
+  }
+
+  pub fn get_api_key(&self) -> &String {
+    &self.api_key
+  }
+
+  pub fn get_emote_name(&self) -> &String {
+    &self.emote_name
+  }
+
+  pub fn get_emote_users(&self) -> &Vec<String> {
+    &self.emote_users
+  }
+}
