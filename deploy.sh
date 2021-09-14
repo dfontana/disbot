b4ac2f1d941cf79cc977f5154baeef62466c175e
dfontana <dyfontana@gmail.com>
2021-09-14 19:11:55 -0400

    Fixed cross compile with GNU over Musl for dynamic linking

--- deploy.sh
+++ deploy.sh
@@ -3,13 +3,9 @@ set -e
 env=${1:-dev}
 host=${2:-raspberrypi}
 
-docker build . -f docker/disbot/Dockerfile -t disbot:latest
-id=$(docker create disbot)
-docker cp $id:/app disbot
-docker rm -v $id
+cross build --release --target armv7-unknown-linux-gnueabihf
 ssh $USER@$host 'mkdir -p ~/deploy'
 scp "$env.env" $USER@$host:~/deploy/
 ssh -t $USER@$host 'sudo systemctl stop '"disbot-$env"
-scp disbot $USER@$host:~/deploy/"disbot-$env"
+scp target/armv7-unknown-linux-gnueabihf/release/disbot $USER@$host:~/deploy/"disbot-$env"
 ssh -t $USER@$host 'sudo systemctl restart '"disbot-$env"
-rm disbot
--- src/main.rs
+++ src/main.rs
@@ -24,7 +24,7 @@ use serenity::{
 };
 use songbird::SerenityInit;
 
-use cmd::{dice_roll::*, help::*, poll::*, server::*, voice::*,  Handler};
+use cmd::{dice_roll::*, help::*, poll::*, server::*, voice::*, Handler};
 use config::Config;
 use env::Environment;
 
