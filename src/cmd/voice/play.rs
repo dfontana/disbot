fa6cf8e64d5f13a70445e44308819f5fb2eac423
dfontana <dyfontana@gmail.com>
2021-09-26 13:16:15 -0400

    Can play via search term or url
    by passing either a URL starting with http or a query to search over. There's no need to quote the arguments as the entire string is joined & searched over.
    
    Addresses #30

--- src/cmd/voice/play.rs
+++ src/cmd/voice/play.rs
@@ -20,18 +20,21 @@ async fn play(ctx: &Context, msg: &Message, args: Args) -> CommandResult {
 
 #[instrument(name = "VoicePlay", level = "INFO", skip(ctx, msg, args))]
 async fn exec_play(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult {
-  // Pull out the URL requested if there is one
-  let maybe_url = args
-    .single::<String>()
-    .map_err(|_| "Must provide a URL")
-    .and_then(|url| match url.starts_with("http") {
-      true => Ok(url),
-      false => Err("Must send a valid URL"),
-    });
-  let url = match maybe_url {
-    Ok(v) => v,
-    Err(s) => {
-      let _ = msg.reply(ctx, s).await;
+  let maybe_args = match args.len() {
+    0 => Err("Must provide a url|search string"),
+    1 => args
+      .single::<String>()
+      .map_err(|_| "Must provide a url|search string"),
+    _ => Ok(args.iter::<String>().fold(String::new(), |mut a, b| {
+      a.push_str(" ");
+      a.push_str(&b.unwrap());
+      a
+    })),
+  };
+  let searchterm = match maybe_args {
+    Ok(v) => v.trim().to_string(),
+    Err(e) => {
+      let _rep = msg.reply(ctx, &format!("{:?}", e)).await;
       return Ok(());
     }
   };
@@ -56,42 +59,50 @@ async fn exec_play(ctx: &Context, msg: &Message, mut args: Args) -> CommandResul
     .await
     .expect("Songbird Voice client placed in at initialisation.")
     .clone();
-  let (handler_lock, success) = manager.join(guild_id, connect_to).await;
+  let (handler_lock, _success) = manager.join(guild_id, connect_to).await;
 
   // Queue up the source
-  let source = match Restartable::ytdl(url, true).await {
-    Ok(source) => source,
+  let is_url = searchterm.starts_with("http");
+  let resolved_src = match is_url {
+    true => Restartable::ytdl(searchterm, true).await,
+    false => Restartable::ytdl_search(searchterm, true).await,
+  };
+
+  let input = match resolved_src {
+    Ok(inp) => Input::from(inp),
     Err(why) => {
       error!("Err starting source: {:?}", why);
       let _ = msg.channel_id.say(&ctx.http, "Error sourcing ffmpeg").await;
       return Ok(());
     }
   };
-  let input = Input::from(source);
 
   let emoji = EmojiLookup::inst().get(guild_id, &ctx.cache).await?;
 
-  let title = input
-    .metadata
+  let metadata = input.metadata.clone();
+  let title = metadata
     .track
-    .as_ref()
-    .unwrap_or(&"<UNKNOWN>".to_string())
+    .or(metadata.title)
+    .unwrap_or("<UNKNOWN>".to_string())
+    .to_string();
+  let source_url = metadata
+    .source_url
+    .unwrap_or("Unknown Source".to_string())
     .to_string();
 
   let mut handler = handler_lock.lock().await;
   handler.set_bitrate(Bitrate::Max);
   handler.enqueue_source(input);
-  let _ = msg
-    .channel_id
-    .say(
-      &ctx.http,
-      MessageBuilder::new()
-        .push_bold("Queued")
-        .push(format!(" ({}) ", handler.queue().len()))
-        .push_mono(title)
-        .mention(&emoji)
-        .build(),
-    )
-    .await;
+
+  let mut build = MessageBuilder::new();
+  build
+    .push_bold("Queued")
+    .push(format!(" ({}) ", handler.queue().len()))
+    .push_mono(title)
+    .mention(&emoji);
+  if !is_url {
+    build.push_line("").push(source_url);
+  }
+  let _ = msg.channel_id.say(&ctx.http, build.build()).await;
   Ok(())
 }
